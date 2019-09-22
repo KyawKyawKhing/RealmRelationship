75295ee566005b0549d0fe9fbef990fe6ea4beec
Kyaw Kyaw Khing <kyawkyawkhing2018@gmail.com>
2019-09-22 21:10:38 +0630

    Initial Commit

---
+++ RealmRelationship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmRelationship
+//
+//  Created by Kyaw Kyaw Khing on 9/22/19.
+//  Copyright © 2019 Kyaw Kyaw Khing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
